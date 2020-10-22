f4072e9cdaef867b943beb66b6c593b10e8437e3
Bahadır KORUMAZ <bkorumaz@havelsan.com.tr>
2020-10-22 13:46:09 +0300

    created nested form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,40 @@
 import React from "react";
+import { useForm, FormProvider, useFormContext } from "react-hook-form";
 
-export default () => (
-  <>
-    <h1>Welcome to React Parcel Micro App!</h1>
-    <p>Hard to get more minimal than this React app.</p>
-  </>
-);
+export default function App() {
+  const methods = useForm();
+  const onSubmit = data => console.log(data);
+
+  return (
+    <FormProvider {...methods} >
+      <h5>Please fill out the form</h5>
+      <form onSubmit={methods.handleSubmit(onSubmit)}>
+        <PersonInfo />
+        <JobInfo />
+        <input type="submit" />
+      </form>
+    </FormProvider>
+  );
+}
+
+function PersonInfo() {
+  const { register } = useFormContext(); // retrieve all hook methods
+  return (
+    <>
+      <label>name</label>
+      <input name="name" ref={register} />
+      <label>surname</label>
+      <input name="surname" ref={register} />
+    </> 
+  );
+}
+
+function JobInfo() {
+  const { register } = useFormContext(); // retrieve all hook methods
+  return (
+    <>
+      <label>job title</label>
+      <input name="title" ref={register} />
+    </>
+  );
+}
